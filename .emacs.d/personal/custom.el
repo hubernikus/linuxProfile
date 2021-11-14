937c05761446ebda6d63568e24b9bf132c902cd5
Lukas Huber <hubernikus@gmail.com>
2021-11-14 16:52:33 +0100

    Update custom.el

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -47,3 +47,55 @@
   ;; (global-display-line-numbers-mode))
 
 (global-linum-mode 1)
+
+
+(setq-default c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode t)
+(setq c-default-style "user") ;; DOES NOT WORK
+(setq c-indentation-style "user")
+
+
+;; Python Hook
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq indent-tabs-mode nil
+                            tab-width 2))))
+
+
+(show-paren-mode 1)
+
+;; Redundant I think
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; Install elpy on command line / linux. Check github/wiki for more info.
+(elpy-enable)
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+  
+  ;; (pyvenv-tracking-mode 1)
+  
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list
+		 (lambda ()
+		   (setq python-shell-interpreter
+				 (concat pyvenv-virtual-env "bin/ipython3")))))
+  
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+				(setq python-shell-interpreter "ipython3"))))
+  )
+
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"))
+
+(setq python-shell-interpreter "ipython"
+	  python-shell-interpreter-args "--simple-prompt -i")
